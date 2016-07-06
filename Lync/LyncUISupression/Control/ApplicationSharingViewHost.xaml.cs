438adc73dd864fdcbaaf8c9c6ac1f53748663e6b
wei <li772555190@live.com>
2016-07-06 15:50:27 +0800

    update  applicationsharingview

--- Lync/LyncUISupression/Control/ApplicationSharingViewHost.xaml.cs
+++ Lync/LyncUISupression/Control/ApplicationSharingViewHost.xaml.cs
@@ -15,78 +15,125 @@ using System.Windows.Shapes;
 
 namespace Lync.Control
 {
-	/// <summary>
-	/// Interaction logic for ApplicationSharingViewHost.xaml
-	/// </summary>
-	public partial class ApplicationSharingViewHost : UserControl
-	{
-		private static ILog _log = LogManager.GetLog(typeof(ApplicationSharingViewHost));
+    /// <summary>
+    /// Interaction logic for ApplicationSharingViewHost.xaml
+    /// </summary>
+    public partial class ApplicationSharingViewHost : UserControl
+    {
+        private static ILog _log = LogManager.GetLog(typeof(ApplicationSharingViewHost));
 
 
 
-		public static DependencyProperty ViewProperty = DependencyProperty.Register("View", typeof(ApplicationSharingView), typeof(ApplicationSharingViewHost), new PropertyMetadata(OnViewPropertyChanged));
-		public ApplicationSharingView View
-		{
-			get { return (ApplicationSharingView)GetValue(ViewProperty); }
-			set { SetValue(ViewProperty, value); }
-		}
+        public static DependencyProperty ViewProperty = DependencyProperty.Register("View", typeof(ApplicationSharingView), typeof(ApplicationSharingViewHost), new PropertyMetadata(OnViewPropertyChanged));
+        public ApplicationSharingView View
+        {
+            get { return (ApplicationSharingView)GetValue(ViewProperty); }
+            set { SetValue(ViewProperty, value); }
+        }
+
+
+        public static DependencyProperty SharingModalityProperty = DependencyProperty.Register("SharingModality", typeof(ApplicationSharingModality), typeof(ApplicationSharingViewHost), new PropertyMetadata(OnSharingModalityPropertyChanged));
+        public ApplicationSharingModality SharingModality
+        {
+            get { return (ApplicationSharingModality)GetValue(SharingModalityProperty); }
+            set { SetValue(SharingModalityProperty, value); }
+        }
+
+
+        public ApplicationSharingViewHost()
+        {
+            InitializeComponent();
+        }
 
 
 
-		public ApplicationSharingViewHost()
-		{
-			InitializeComponent();
-		}
+        #region UI Events
+
+        //private void OnVideoPanelLayout(object sender, System.Windows.Forms.LayoutEventArgs e)
+        //{
+        //	if (VideoWindowFeed != null)
+        //	{
+        //		VideoWindowFeed.SetWindowPosition(0, 0, VideoWidth, VideoHeight);
+        //	}
+        //}
+
+        #endregion
+
+        #region Property setter callbacks
 
 
 
-		#region UI Events
+        private static void OnViewPropertyChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            var thisControl = (ApplicationSharingViewHost)sender;
+            var view = (ApplicationSharingView)args.NewValue;
 
-		//private void OnVideoPanelLayout(object sender, System.Windows.Forms.LayoutEventArgs e)
-		//{
-		//	if (VideoWindowFeed != null)
-		//	{
-		//		VideoWindowFeed.SetWindowPosition(0, 0, VideoWidth, VideoHeight);
-		//	}
-		//}
+            if (view != null)
+            {
+                thisControl.ShowApplicationSharingView(thisControl.applicaionViewPanel, view);
+            }
+        }
 
-		#endregion
+        public static void OnSharingModalityPropertyChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
 
-		#region Property setter callbacks
-
-
-
-		private static void OnViewPropertyChanged(object sender, DependencyPropertyChangedEventArgs args)
-		{
-			var thisControl = (ApplicationSharingViewHost)sender;
-			var view = (ApplicationSharingView)args.NewValue;
-
-			if (view != null)
-			{
-				ShowApplicationSharingView(thisControl.applicaionViewPanel, view);
-			}
-		}
+        }
 
 
-		private static void ShowApplicationSharingView(System.Windows.Forms.Panel applicaionViewPanel, ApplicationSharingView view)
-		{
 
-			_log.Debug("ShowApplicationSharingView  applicaionViewPanel:{0}", applicaionViewPanel.Handle.ToInt32());
+        private void ShowApplicationSharingView(System.Windows.Forms.Panel applicaionViewPanel, ApplicationSharingView view)
+        {
 
-			try
-			{
-				//sets the properties required for the native video window to draw itself
-				view.SetParent(applicaionViewPanel.Handle.ToInt32());
-			}
-			catch (Exception exception)
-			{
-				_log.ErrorException("", exception);
-			}
-		}
+            _log.Debug("ShowApplicationSharingView  applicaionViewPanel:{0}", applicaionViewPanel.Handle.ToInt32());
 
-
-		#endregion
+            try
+            {
+                view.PropertyChanged += OnViewPropertyChanged;
+                view.SetParent(applicaionViewPanel.Handle.ToInt32());
+                if (view.Properties == null)
+                {
+                    return;
+                }
 
 
-	}
+
+            }
+            catch (Exception exception)
+            {
+                _log.ErrorException("", exception);
+            }
+        }
+
+
+        private void OnViewPropertyChanged(object sender, ApplicationSharingViewPropertyChangedEventArgs e)
+        {
+            ApplicationSharingView view = (ApplicationSharingView)sender;
+            if (view.Properties == null)
+            {
+                return;
+            }
+
+            //If the changed viewer property is a dimension property then resize parent container control
+            if (e.Property == ApplicationSharingViewProperty.Height || e.Property == ApplicationSharingViewProperty.Width)
+            {
+                //If user chose FitToParent, the parent container control is not resized. 
+                if (SharingModality.View.DisplayMode == ApplicationSharingViewDisplayMode.FitToParent)
+                {
+                    return;
+                }
+            }
+            else if (e.Property == ApplicationSharingViewProperty.ParentWindow)
+            {
+                if (SharingModality.View.State == ApplicationSharingViewState.Active)
+                {
+                    SharingModality.View.SyncRectangle();
+                }
+
+            }
+        }
+
+        #endregion
+
+
+    }
 }
